b852320098e4c39c6a0a7ea6d3caa85545a13805
Alwin Hughes <alwin.hughes@btopenworld.com>
2020-03-26 09:10:23 +0000

    five 2 eighting now picks a user to mention. also added some stuff for limiting what messages the bot can respond to and where it will message

--- bot.js
+++ bot.js
@@ -3,7 +3,9 @@ var auth = require('./auth.json');
 
 var cron = require('cron');
 
+var five_to_eight_channel_id;// = "692462958562902038";
 
+var valid_channels = [];
 
 var bot = new Discord.Client({
   token: auth.token,
@@ -12,14 +14,37 @@ var bot = new Discord.Client({
 });
 
 bot.on('ready', function() {
-  console.log('Logged in as %s', bot.user.tag);
+
+  //getting channel id
+  bot.channels.cache.forEach(
+    x => {
+      if(x.name == 'general') {
+        five_to_eight_channel_id = x.id;
+        valid_channels.push(x.id);
+      }
+    }
+  );
 });
 
 var five_to_eight = (() => {
 
-  var channel = bot.channels.cache.get("692462958562902038");
+  var users = [];
+
+  bot.users.cache.forEach( x => {
+
+    if(!x.bot) {
+      users.push(x.id);
+    }
+
+  });
+
+
+  var user = users[getRandInt(users.length)];
+
+
+  var channel = bot.channels.cache.get(five_to_eight_channel_id);
+  channel.send("Congratulations <@" + bot.users.cache.get(user).id+ '>! you are today\'s special guest');
   channel.send("it is " + getFiveToEight());
-  channel.send("Congratulations you are todays special guest!");
   channel.send("No one likes you");
   channel.send("No one likes you");
   channel.send("Woa-ooo-ooo");
@@ -27,24 +52,59 @@ var five_to_eight = (() => {
 
 }).bind(this);
 
+var test_event = ( () => {
+  var users = [];
+
+  bot.users.cache.forEach( x => {
+
+    if(!x.bot) {
+      users.push(x.id);
+    }
+
+  });
+
+
+  var user = users[getRandInt(users.length)];
+
+//  console.log("Congratulations " + bot.users.cache.get(user).username + '! you are today\'s special guest');
+
+  var channel = bot.channels.cache.get(five_to_eight_channel_id);
+  channel.send("Congratulations <@" + bot.users.cache.get(user).id+ '>! you are today\'s special guest');
+
+
+}).bind(this);
+
+function getRandInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
 
 function getFiveToEight() {
-  return five_2_eight[Math.floor(Math.random() * Math.floor(five_2_eight.length-1))];
+  return five_2_eight[getRandInt(five_2_eight.length)];
 }
 
 let job = new cron.CronJob('00 55 07,19 * * *', five_to_eight); // fires every day, at 01:05:01 and 13:05:01
+let job1 = new cron.CronJob('00,30 * * * * *', test_event); // fires every day, at 01:05:01 and 13:05:01
 
 job.start();
+//job1.start();
 
 
 const five_2_eight = ["528", "five to eight", "7:55", "19:55"]
 
 bot.on('message', function(msg) {
 
-  console.log(msg.content.indexOf("when"));
+  console.log(msg);
+
+  //checks if the channel is valid
+  if(valid_channels.indexOf(msg.channel.id) === -1) {
+    return;
+  }
+
+  if(msg.author.bot) {
+    return;
+  }
 
   if (msg.content.indexOf("when") !== -1) {
-    console.log('sending message')
     msg.channel.send('At ' + getFiveToEight());
   }
 
